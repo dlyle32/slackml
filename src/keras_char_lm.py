9097704220d8fa4c49d6c4677b627dd0574dc512
dlyle32 <davidlyle@Davids-MacBook-Air-3.local>
2020-09-24 17:52:38 -0400

    splitting text into equal size batches across messages, truncating on newline

--- src/keras_char_lm.py
+++ src/keras_char_lm.py
@@ -30,14 +30,14 @@ def create_model(chars, n_a, maxlen, lr):
     print(model.summary())
     return model
 
-def sample(model, chars, temperature=1.0):
+def sample(model, chars, char_to_ix, temperature=1.0):
     maxlen=model.layers[0].input_shape[1]
     vocab_size =model.layers[-1].output_shape[-1]
     output = ""
     x = np.zeros((1, maxlen, vocab_size))
     char_index = -1
     i = 0
-    while char_index != vocab_size-1 and i < maxlen:
+    while char_index != char_to_ix['\n'] and i < maxlen:
         preds = model.predict(x, verbose=0)[0][i]
         preds = np.asarray(preds).astype('float64')
         preds = np.log(preds) / temperature
@@ -56,11 +56,11 @@ def get_ix_from_char(char_to_ix, chars, c):
     else:
         return char_to_ix["<UNK>"]
 
-def on_epoch_end(epoch, model, chars, metrics):
+def on_epoch_end(epoch, model, chars, char_to_ix, metrics):
     print("COMPLETED EPOCH %d" % epoch)
     for i,lbl in enumerate(model.metrics_names):
         print(lbl + ": " + str(metrics[i]))
-    sample_msg = sample(model, chars) 
+    sample_msg = sample(model, chars, char_to_ix)
     print(sample_msg)
 
 def oh_to_char(chars, oh):
@@ -68,46 +68,46 @@ def oh_to_char(chars, oh):
     return "0" if len(char) == 0 else char[0]
 
 def main(datadir):
-    mini_batch_size = 10
+    mini_batch_size = 128
     n_a = 512
-    num_epochs = 200
+    num_epochs = 1
     train, test = load_datasets(datadir)
     m = len(train)
-    numbatches = math.floor(m / mini_batch_size)
-    if m % mini_batch_size != 0:
-        numbatches += 1
     chars = set()
     maxlen = 60
-    #maxlen = max([len(msg) for msg in train]) + 1
-    #maxlen = reduce(lambda a,b: len(a) if len(a) > b else b, train) + 1 # Adding one for appended <EOM>
     #for msg in train:
     #    chars = chars.union(set(msg))
     #chars = sorted(list(chars))
-    chars = ['\t', '\n', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '<UNK>', '<EOM>']
+    chars = ['\t', '\n', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '<UNK>']
     char_to_ix = {c: i for i, c in enumerate(chars)}
 
     model = create_model(chars, n_a, maxlen, 0.01)
     metrics = []
+    train = train[:10]
     for e in range(0,num_epochs):
         random.shuffle(train)
-        for b in range(0,numbatches):
+        data = "".join(train)
+        nummsgs = math.floor(len(data) / maxlen)
+        if len(data) % maxlen != 0:
+            nummsgs += 1
+        numbatches = math.floor(nummsgs / mini_batch_size)
+        if nummsgs % mini_batch_size != 0:
+            numbatches += 1
+        for b in range(numbatches):
             X = np.zeros((mini_batch_size, maxlen, len(chars)))
             Y = np.zeros((mini_batch_size, maxlen, len(chars)))
-            for i, msg in enumerate(train[b*mini_batch_size:min(m, (b+1)*mini_batch_size)]):
-                for t,c in enumerate(msg[0:maxlen-1]):
-                    char_index = get_ix_from_char(char_to_ix, chars,c)
-                    X[i, t+1, char_index] = 1
-                    Y[i, t, char_index] = 1
-                Y[i, min(t+1,maxlen-1), char_to_ix["<EOM>"]] = 1
-                print(msg)
-                for j in range(maxlen):
-                    print("X: " + oh_to_char(chars, X[i,j,:]))
-                    print("Y: " + oh_to_char(chars, Y[i,j,:]))
-                return
+            msgs = 0
+            for i in range(b* maxlen * mini_batch_size,min(len(data), (b+1)*maxlen *mini_batch_size), maxlen):
+                last_ix = min(i+maxlen-1, len(data)-1)
+                for t, c in enumerate(data[i:last_ix]):
+                    char_index = get_ix_from_char(char_to_ix, chars, c)
+                    X[msgs, t + 1, char_index] = 1
+                    Y[msgs, t, char_index] = 1
+                Y[msgs, t, get_ix_from_char(char_to_ix, chars, data[last_ix])] = 1
+                msgs += 1
             metrics = model.train_on_batch(X,Y)
-            #print("FINISHED BATCH %d on slice [%d:%d]" % (b, b*mini_batch_size,min(m, (b+1)*mini_batch_size)))
         model.save("/slackml/mymodel5.keras")
-        on_epoch_end(e, model, chars, metrics)
+        on_epoch_end(e, model, chars,char_to_ix, metrics)
 
 if __name__=="__main__":
     main("/slackdata/")
