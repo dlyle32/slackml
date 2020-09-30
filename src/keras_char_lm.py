added gradient clipping, model saving, simplified char set, fixed truncated character in training

src/keras_char_lm.py
@@ -24,9 +24,9 @@ def create_model(chars, n_a, maxlen, lr):
     model = Sequential([
         LSTM(n_a, input_shape=(maxlen, vocab_size), return_sequences=True),
         LSTM(n_a, input_shape=(maxlen, n_a), return_sequences=True),
-        TimeDistributed(Dense(vocab_size, activation="softmax"))
+        Dense(vocab_size, activation="softmax")
     ])
-    opt = Adam(learning_rate=lr)
+    opt = Adam(learning_rate=lr, clipvalue=5)
     model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=["accuracy"])
     print(model.summary())
     return model
@@ -40,12 +40,13 @@ def sample(model, chars, char_to_ix, temperature=1.0):
     i = 0
     while char_index != char_to_ix['\n'] and i < maxlen:
         preds = model.predict(x, verbose=0)[0][i]
-        preds = np.asarray(preds).astype('float64')
-        preds = np.log(preds) / temperature
-        exp_preds = np.exp(preds)
-        preds = exp_preds / np.sum(exp_preds)
-        probas = np.random.multinomial(1, preds, 1)
-        char_index = np.argmax(probas)
+        #preds = np.asarray(preds).astype('float64')
+        #preds = np.log(preds) / temperature
+        #exp_preds = np.exp(preds)
+        #preds = exp_preds / np.sum(exp_preds)
+        char_index = np.random.choice(range(vocab_size), p = preds.ravel())
+        #probas = np.random.multinomial(1, preds, 1)
+        #char_index = np.argmax(probas)
         x[0,i,char_index] = 1
         output += chars[char_index]
         i+=1
@@ -57,10 +58,11 @@ def get_ix_from_char(char_to_ix, chars, c):
     else:
         return char_to_ix["<UNK>"]
 
-def on_epoch_end(epoch, model, chars, char_to_ix, metrics):
+def on_epoch_end(epoch, model, chars, char_to_ix, metrics, model_path):
     #print("COMPLETED EPOCH %d" % epoch)
     #for lbl in metrics.keys():
     #    print(lbl + ": " + str(metrics[lbl]))
+    model.save(model_path)
     sample_msg = sample(model, chars, char_to_ix)
     print("\n" + sample_msg)
 
@@ -71,7 +73,7 @@ def oh_to_char(chars, oh):
 def main(datadir, args):
     model_path = "/slackml/mymodel3.keras"
     mini_batch_size = 512
-    n_a = 256
+    n_a = 128
     num_epochs = 60
     train, test = load_datasets(datadir)
     m = len(train)
@@ -80,7 +82,8 @@ def main(datadir, args):
     #for msg in train:
     #    chars = chars.union(set(msg))
     #chars = sorted(list(chars))
-    chars = ['\t', '\n', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '<UNK>']
+    #chars = ['\t', '\n', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '<UNK>']
+    chars = ['\n', ' ', '!', '"', ',', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '?', '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '~', '<UNK>']
     char_to_ix = {c: i for i, c in enumerate(chars)}
 
     if args.loadmodel:
@@ -97,14 +100,15 @@ def main(datadir, args):
     Y = np.zeros((nummsgs, maxlen, len(chars)))
     msgs = 0
     for i in range(0,len(data), maxlen):
-        last_ix = min(i+maxlen-1, len(data)-1)
+        last_ix = min(i+maxlen, len(data)-1)
         for t,c in enumerate(data[i:last_ix]):
             char_index = get_ix_from_char(char_to_ix, chars, c)
-            X[msgs, t + 1, char_index] = 1
+            if t < maxlen-1:
+                X[msgs, t + 1, char_index] = 1
             Y[msgs, t, char_index] = 1
-        Y[msgs, t, get_ix_from_char(char_to_ix, chars, data[last_ix])] = 1
+        Y[msgs, maxlen-1, get_ix_from_char(char_to_ix, chars, data[last_ix])] = 1
         msgs+=1
-    epoch_callback = LambdaCallback(on_epoch_end=lambda epoch, logs: on_epoch_end(epoch, model, chars, char_to_ix, logs))
+    epoch_callback = LambdaCallback(on_epoch_end=lambda epoch, logs: on_epoch_end(epoch, model, chars, char_to_ix, logs, model_path))
     model.fit(X,Y,
               batch_size=mini_batch_size,
               epochs=num_epochs,