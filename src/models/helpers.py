0c316153e57e7979f0a63b91f32d59086e19f775
dlyle32 <lyledavidt@gmail.com>
2020-10-29 16:48:16 -0400

    helper function for right padding sequences with a character

--- src/models/helpers.py
+++ src/models/helpers.py
@@ -14,3 +14,6 @@ def token_to_oh(index, vocab_size):
     x = np.zeros((vocab_size))
     x[index] = 1
     return x
+
+def char_padded(sequence, pad, maxlen):
+    return [pad if i >= len(sequence) else sequence[i] for i in range(maxlen)]
