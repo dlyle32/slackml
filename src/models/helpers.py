85151c2512b508454ee03659a009b805617b6a6e
dlyle32 <lyledavidt@gmail.com>
2020-12-30 11:20:34 -0500

    adding create_oh function to convert numpy arrays of ints to one hot encoding

--- src/models/helpers.py
+++ src/models/helpers.py
@@ -15,5 +15,18 @@ def token_to_oh(index, vocab_size):
     x[index] = 1
     return x
 
+def create_oh(int_array, vocab_size):
+    oh_shape = int_array.shape + (vocab_size,)
+    rows = int_array.shape[-2]
+    cols = int_array.shape[-1]
+
+    rindices = np.arange(rows).reshape(rows,1)
+    cindices = np.arange(cols).reshape(1,cols)
+
+    oh = np.zeros(oh_shape)
+
+    oh[rindices, cindices, int_array] = 1
+    return oh
+
 def char_padded(sequence, pad, maxlen):
     return [pad if i >= len(sequence) else sequence[i] for i in range(maxlen)]
