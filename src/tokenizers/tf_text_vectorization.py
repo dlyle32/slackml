2838b6d5f011b7f61a6c52993427805d1cce94f2
Ubuntu <ubuntu@ip-172-31-2-188.us-east-2.compute.internal>
2021-03-08 23:45:01 +0000

    share vectorize layer, return vocab correctly

--- src/tokenizers/tf_text_vectorization.py
+++ src/tokenizers/tf_text_vectorization.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+import string
 
 
 def custom_standardization(input_string):
@@ -8,7 +9,7 @@ def custom_standardization(input_string):
     stripped_html = tf.strings.regex_replace(lowercased, "<br />", " ")
     return tf.strings.regex_replace(stripped_html, f"([{string.punctuation}])", r" \1")
 
-def prepare_lm_inputs_labels(text):
+def prepare_lm_inputs_labels(text, vectorize_layer):
     """
     Shift word sequences by 1 position so that the target for position (i) is
     word at position (i+1). The model will use all words up till position (i)
@@ -30,21 +31,22 @@ class TFVectTokenizer:
         self.seqlen = seqlen
         self.step = step
         self.vocab_size = 20000
-
-    def tokenize(self, text_ds):
-        # Create a vectorization layer and adapt it to the text
-        vectorize_layer = TextVectorization(
+        self.vectorize_layer = TextVectorization(
             standardize=custom_standardization,
             max_tokens=self.vocab_size - 1,
             output_mode="int",
             output_sequence_length=self.seqlen + 1,
         )
-        vectorize_layer.adapt(text_ds)
-        vocab = vectorize_layer.get_vocabulary()  # To get words back from token indices
+
+    def tokenize(self, text_ds):
+        # Create a vectorization layer and adapt it to the text
+        
+        self.vectorize_layer.adapt(text_ds)
+        vocab = self.vectorize_layer.get_vocabulary()  # To get words back from token indices
         reverse_token_map = {t: i for i, t in enumerate(vocab)}
-        return vocab, text_ds, reverse_token_map
+        return text_ds, vocab, reverse_token_map
 
     def get_input_sequences(self, text_ds, reverse_token_map):
-        text_ds = text_ds.map(prepare_lm_inputs_labels)
+        text_ds = text_ds.map(lambda text : prepare_lm_inputs_labels(text, self.vectorize_layer))
         text_ds = text_ds.prefetch(tf.data.experimental.AUTOTUNE)
         return text_ds
